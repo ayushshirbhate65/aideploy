02b1d58b695609bce285ef80f3f8fc2acccae331
Ayush Shirbhate <ayushshirbhate65@gmail.com>
2026-03-06 23:07:17 +0530

    global CORS config

--- src/main/java/com/demo/services/QnaService.java
+++ src/main/java/com/demo/services/QnaService.java
@@ -1,44 +1,36 @@
 package com.demo.services;
 
+import com.demo.client.ProductApiClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class QnaService {
-
     private final GeminiService geminiService;
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final ProductApiClient productApiClient;
 
-    public QnaService(GeminiService geminiService) {
+    public QnaService(GeminiService geminiService, ProductApiClient productApiClient) {
         this.geminiService = geminiService;
+        this.productApiClient = productApiClient;
     }
 
     public String answerQuestion(String question) {
-
-        String products =
-            restTemplate.getForObject(
-                "http://localhost:8080/api/products",
-                String.class
-            );
+        String products = productApiClient.fetchProductsJson();
 
         String prompt = """
             Product data:
             %s
-
             Question:
             %s
-
             Answer ONLY in md format. with little sentences and mentioning types where required.
-            
-            
+
             But any question related to fertilzers and what to do with any produce are fine.
             Answer the question realated to originPlace . Based on its quality, name, focus on description for extra information. Answer anything that we get as api response.
-        	For question that make you list things, use numbering like 1,2...
-        	
-        	Make sure to give proper spaces/line breaks in the answer.
-        	
-        	NOTE IMPORTANT: Any question that you feel is not related to products should be answered by : Not Relevent. 
-            
+            For question that make you list things, use numbering like 1,2...
+
+            Make sure to give proper spaces/line breaks in the answer.
+
+            NOTE IMPORTANT: Any question that you feel is not related to products should be answered by : Not Relevent.
+
             """.formatted(products, question);
 
         return geminiService.askGemini(prompt);
